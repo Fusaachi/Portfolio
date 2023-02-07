55f6a20ddfb2dbcf55bf5ed433edac3168848cf8
Fusaachi <g.pauline91@outlook.com>
2023-02-07 15:22:14 +0100

    Add createSkill

--- admin/ajoutCompetence.php
+++ admin/ajoutCompetence.php
@@ -1,44 +1,56 @@
 <?php
 
-session_start();
-define("PAGE_TITLE", "Ajout Competence");
+define("PAGE_TITLE", "Ajout d'une Compétence");
 
-require_once("../controllers/accountController.php");
+require_once("../controllers/skillController.php");
 
+$skillController = new SkillController;
 
-$accountController = new AccountController;
-// Permet de vérifier que l'utilisateur soit connecté
-$accountController->isLogged();
+if(isset($_POST["submit"])){
+    $error = $skillController->createSkill($_POST["name"], $_POST["level"], $_FILES["picture"]);
+}
+
 
 ?>
 <?php include("../assets/inc/head.php"); ?>
 <?php include("../assets/inc/header.php"); ?>
 
-<main class="container-fluid mt-5 mb-5">
-    <form action="submit" method="post">
-            <h1 class="text-center"> Ajout d'une nouvelle compétence</h1>
-
-            <label for="name" >Nom :</label>
-            <input id="name">
-
-            <label for="level" >Niveau :</label>
-            <input type="text" id="level">
-        </div>
-        <div>
-            <label for="picture" >Image :</label>
-            <input type="file" id="picture">
-        </div>
-        <div >
-                <button type="submit" name= "submit" class="btn btnProject mt-4 mb-5">Ajouter</button>
+<main class="mt-5 mb-5">
+    
+    <h1 class="text-center mb-5"> Ajout d'une nouvelle compétence</h1>
+    <?php 
+    if(isset($error)){
+        if($error["success"]){?>
+            <div class="alert alert-success"><?=$error["message"]?></div>
+       <?php } else {?>
+        <div class="alert alert-danger"><?=$error["message"]?></div>
+     <?php  }
+    }
+    ?>
+    <div class="container d-flex justify-content-center  ">
+        <form action="#" method="POST" enctype="multipart/form-data">     
+            <div class="mb-2">
+                <label for="name" >Nom :</label>
+                <input type="text" name="name" id="name" required>
+            </div>
+            <div class="mb-2">
+                <label for="level" >Niveau :</label>
+                <input type="number" min="1" max="5" name="level" id="level" required>
+            </div>
+            <div>
+                <label for="picture" >Image :</label>
+                <input type="file" id="picture" name="picture" accept="image/png, image/jpg, image/webp" required>
+            </div>
+            <div >
+                <button type="submit" name="submit" class="btn btnProject mt-4 mb-5">Ajouter</button>
             </div>
 
 
-    </form>
-    <div>
-        <a href="../admin/index.php" class="btn btnProject">Retour</a>
+        </form>
     </div>
-
-
+        <div class="text-center">
+            <a href="../admin/index.php" class="btn btnProject">Retour</a>
+        </div>
 
 </main>
 <?php include("../assets/inc/footer.php");
--- controllers/skillController.php
+++ controllers/skillController.php
@@ -1,7 +1,8 @@
 <?php
-require_once("./conf/conf.php");
-require_once("./models/skillModel.php");
+require_once(__DIR__ . "/../conf/conf.php");
+require_once(__DIR__ . "/../models/skillModel.php");
 class SkillController {
+
     // TODO: créer les méthodes permettant des récupérer les skills (readAll()...)
     function readAll() : array 
     {
@@ -34,9 +35,52 @@ class SkillController {
         $skill->projects = $statement->fetchAll(PDO::FETCH_CLASS, "SkillModel");
     }
 
-    public function createSkill()
+    public function createSkill(string $name, int $level, array $picture) 
     {
+        if(strlen($name)> 255)
+        {
+            return [
+                "success" => false,
+                "message" => "Le nom doit contenir 255 caractère"
+            ];
+        }
+        if($level < 1 || $level > 5)
+        {
+            return [
+                "success" => false,
+                "message" => "Le niveau doit être compris entre 1 et 5"
+            ];
+        }
 
+        if(!in_array($picture["type"],["image/png", "image/jpeg", "image/webp"]))
+        {
+            return [
+                "success" => false,
+                "message" => "Formats d'image acceptés :  Png, Jpeg, Webp"
+            ];
+        }
+
+        // Les informations sont correctes : stockons l'image en lui attribuant un nouveau nom unique
+        $image_name = time() . $picture["name"];
+        move_uploaded_file($picture["tmp_name"], __DIR__ . "/../assets/img/skills/" . $image_name);
+            
+         // L'image a bien été stockée, exécutons la requête pour ajouter la compétence
+        global $pdo;
+
+        $sql = "INSERT INTO skill ( name, level, picture)
+                VALUES ( :name, :level, :picture)";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(":name", $name);
+        $statement->bindParam(":level", $level);
+        $statement->bindParam(":picture", $image_name);
+
+        $statement->execute();
+
+
+        return [
+            "success" => true,
+            "message" => "La compétence a été crée"
+        ];
     }
 }
 
